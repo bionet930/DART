0546ae63f296a22b7c4c888a6b6903d097fa3d1e
Hugo Muñoz <bionet930@gmail.com>
2021-09-28 22:47:56 -0300

    Prueba Flutter

---
+++ contador/lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Center(
+        child: Text('Hola Mundo!'),
+      ),
+    );
+
+  }
+
+}
