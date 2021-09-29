4c0b3e77187fe78e32766370bf423960fef12420
Hugo Muñoz <bionet930@gmail.com>
2021-09-28 22:46:34 -0300

    Prueba Flutter

---
+++ contador/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+//Importaciones de los archivos
+import 'src/app.dart';
+
+
+void main() {
+
+runApp(MyApp());
+
+
+}
+
+
+
