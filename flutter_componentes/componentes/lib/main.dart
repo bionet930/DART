0ad18ebc7c1a919a1217cae850e892db076de467
Hugo Muñoz <bionet930@gmail.com>
2021-10-07 23:51:26 -0300

    Nueva App con Mas Funciones

---
+++ flutter_componentes/componentes/lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:componentes/src/pages/home_temp.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+      home: HomePageTemp()
+    );
+  }
+}
