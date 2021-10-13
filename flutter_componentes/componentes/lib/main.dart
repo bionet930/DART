12dbc3e29aaed040b3b54a53b7cc92fddd72d3fd
Hugo Muñoz <bionet930@gmail.com>
2021-10-12 23:30:42 -0300

    from JSONS

--- flutter_componentes/componentes/lib/main.dart
+++ flutter_componentes/componentes/lib/main.dart
@@ -1,6 +1,7 @@
-
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
+//import 'package:componentes/src/pages/home_temp.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
